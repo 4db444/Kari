a310e0ecba566c81fa9431eef6d7129e9a678ccf
4db444 <brahim77990alhyan@gmail.com>
2026-01-03 21:25:50 +0100

    create the database config

---
+++ config/db.example.php
@@ -0,0 +1,9 @@
+<?php
+    // rename this file to db.php
+    return [
+        "driver" => "",             // PDO driver (mysql, sqlite, ...)
+        "host" => "",               // host name
+        "dbname" => "",             // database name
+        "user" => "",               // database user 
+        "password" => ""            // database password
+    ];
