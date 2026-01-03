6cbf54cc2b6042f6b962a570fdcf3a8e4ca964d9
4db444 <brahim77990alhyan@gmail.com>
2026-01-03 23:32:10 +0100

    create teh user entity

---
+++ entities/User.php
@@ -0,0 +1,22 @@
+<?php
+    namespace Entities;
+
+    class User {
+        public function __construct(
+            private int $id,
+            public string $first_name,
+            public string $last_name,
+            public string $email,
+            private string $password
+        ){}
+
+        public static function UserFromArray (array $user){
+            return new self(
+                $user->id,
+                $user->first_name,
+                $user->last_name,
+                $user->email,
+                $user->password
+            );
+        }
+    }
