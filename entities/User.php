2dc2117956e8f603d601787bb9a9a4bdb29536d1
4db444 <brahim77990alhyan@gmail.com>
2026-01-04 14:10:05 +0100

    adding role (is_admin) property to the class

--- entities/User.php
+++ entities/User.php
@@ -7,16 +7,18 @@
             public string $first_name,
             public string $last_name,
             public string $email,
-            private string $password
+            private string $password,
+            public bool $is_admin
         ){}
 
         public static function UserFromArray (array $user){
             return new self(
-                $user->id,
-                $user->first_name,
-                $user->last_name,
-                $user->email,
-                $user->password
+                $user["id"],
+                $user["first_name"],
+                $user["last_name"],
+                $user["email"],
+                $user["password"],
+                $user["is_admin"]
             );
         }
     }
