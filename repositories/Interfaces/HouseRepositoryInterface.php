216b1211ecb9e85df7bc6675a3ab41cad7acac38
4db444 <brahim77990alhyan@gmail.com>
2026-01-05 14:59:57 +0100

    create the house repository interface

---
+++ repositories/Interfaces/HouseRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+    namespace Repositories\Interfaces;
+
+    require_once __DIR__ . "/../../vendor/autoload.php";
+
+    use Entities\House;
+
+    interface HouserepositoryInterface {
+        public function find(int $id) : ?House;
+        public function save(House $house);
+    }
