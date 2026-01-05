d8b7e58454488dda0d8d8079fb51efd86eb60c9b
4db444 <brahim77990alhyan@gmail.com>
2026-01-05 12:22:19 +0100

    create the house entity

---
+++ entities/House.php
@@ -0,0 +1,36 @@
+<?php
+    namespace Entities;
+
+    require_once __DIR__ . "/../vendor/autoload.php";
+
+    class House {
+        public function __construct (
+            private int $id,
+            public string $city,
+            public string $address,
+            public int $room_number,
+            public int $max_guests,
+            public float $price,
+            private int $user_id
+        ){}
+
+        public static function HouseFromArray(array $house){
+            return new self(
+                $house["id"],
+                $house["city"],
+                $house["address"],
+                $house["room_number"],
+                $house["max_guests"],
+                $house["price"],
+                $house["user_id"],
+            );
+        }
+
+        public function get_id (){
+            return $this->id;
+        }
+
+        public function set_id (int $id){
+            if ($id > 0) $this->id = $id;
+        }
+    }
