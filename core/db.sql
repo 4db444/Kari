098047e8b37f45cc84d1537d1ef934d46f031ed1
4db444 <brahim77990alhyan@gmail.com>
2026-01-08 12:41:13 +0100

    update the sql structure - adding address, is_canceled, rating

--- core/db.sql
+++ core/db.sql
@@ -16,7 +16,8 @@ DROP TABLE IF EXISTS houses;
 CREATE TABLE IF NOT EXISTS houses(
     id INT PRIMARY KEY AUTO_INCREMENT,
     city VARCHAR(50) NOT NULL,
-    room_number INT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    total_rooms INT NOT NULL,
     max_guests INT NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     owner INT NOT NULL,
@@ -37,14 +38,15 @@ CREATE TABLE IF NOT EXISTS images (
         ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS reservations;
 CREATE TABLE IF NOT EXISTS reservation(
     id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     house_id INT NOT NULL,
     `from` DATE NOT NULL,
     `to` DATE NOT NULL,
-    rating INT CHECK (rating >= 1 AND rating <= 5),
+    is_canceled BOOLEAN NOT NULL DEFAULT 0,
+    rating INT CHECK (rating >= 1 AND rating <= 5) DEFAULT NULL,
 
     FOREIGN KEY (user_id)
         REFERENCES users(id)
@@ -67,3 +69,19 @@ CREATE TABLE IF NOT EXISTS favorites(
         REFERENCES  houses(id)
         ON DELETE CASCADE
 );
+
+select * from reservation;
+
+insert into reservation values (null, 5, 1, '2026-01-01', '2026-01-10', 4);
+insert into reservation values (null, 5, 1, '2026-01-05', '2026-01-15', 4);
+
+insert into houses values(null, 'xx', 44, 44, 44, 5);
+select * from houses;
+select CURRENT_DATE;
+
+select * from reservation;
+
+select count (*)
+from reservation
+where house_id = 1 
+and ((`to` BETWEEN "2026-01-19" and "2026-01-20") or (`from` between "2026-01-19" and "2026-01-20"));
